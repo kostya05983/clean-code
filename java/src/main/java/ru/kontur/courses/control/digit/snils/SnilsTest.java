bc35909d2b34a1f69837955482d2b6665f24f8b9
kostya05983 <kostya05983@mail.ru>
2023-06-30 19:05:41 +0700

    Поправили ещё один тест

--- java/src/main/java/ru/kontur/courses/control/digit/snils/SnilsTest.java
+++ java/src/main/java/ru/kontur/courses/control/digit/snils/SnilsTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SnilsTest {
     @ParameterizedTest
-    @CsvSource(value = {"1,1", "10,2", "100,3", "10001,5", "1111,10", "112233445,95", "87654303,0", "87654302,0", "116973385,89",
+    @CsvSource(value = {"1,1", "10,2", "100,3", "1001,5", "1111,10", "112233445,95", "87654303,0", "87654302,0", "116973385,89",
             "152675138,70", "463436384,96", "158757369,28", "192168000,62"})
     public void shouldCalculateSnilsSuccess(long input, long expected) {
         assertEquals(expected, Snils.calculateSnils(input));
